afe9c2dd42916800c211b5c79c328c1e0a73557e
shahriyardx <mdshahriyaralam552@gmail.com>
2025-09-19 00:22:00 +0600

    fix: don't show duplicate tabs

--- src/hooks/useRecentTabs.ts
+++ src/hooks/useRecentTabs.ts
@@ -4,20 +4,21 @@ import { db } from "@/lib/db"
 const useRecentTabs = () => {
 	const tabs =
 		useLiveQuery(() => db.recenttabs.orderBy("id").reverse().toArray()) || []
-	const filteredTabs = tabs
-		.filter(
-			(tab) =>
-				tab.url &&
-				tab.url.length > 1 &&
-				!tab.url.startsWith("about:") &&
-				!tab.url.startsWith("chrome://") &&
-				!tab.url.startsWith("view-source:") &&
-				!tab.title.startsWith("https://") &&
-				!tab.title.startsWith("http://"),
-		)
-		.slice(0, 10)
+	const filteredTabs = tabs.filter(
+		(tab) =>
+			tab.url &&
+			tab.url.length > 1 &&
+			!tab.url.startsWith("about:") &&
+			!tab.url.startsWith("chrome://") &&
+			!tab.url.startsWith("view-source:") &&
+			!tab.title.startsWith("https://") &&
+			!tab.title.startsWith("http://"),
+	)
+	const uniqueTabs = Array.from(
+		new Map(filteredTabs.map((tab) => [tab.url, tab])).values(),
+	).slice(0, 10)
 
-	return { tabs: filteredTabs }
+	return { tabs: uniqueTabs }
 }
 
 export default useRecentTabs
