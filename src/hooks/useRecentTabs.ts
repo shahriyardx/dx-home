8465baa089732ba76e9e42e3814bc09d64ecec5f
shahriyardx <mdshahriyaralam552@gmail.com>
2025-09-18 16:26:52 +0600

    chore: show last 10 closed tabs

--- src/hooks/useRecentTabs.ts
+++ src/hooks/useRecentTabs.ts
@@ -4,16 +4,18 @@ import { db } from "@/lib/db"
 const useRecentTabs = () => {
 	const tabs =
 		useLiveQuery(() => db.recenttabs.orderBy("id").reverse().toArray()) || []
-	const filteredTabs = tabs.filter(
-		(tab) =>
-			tab.url &&
-			tab.url.length > 1 &&
-			!tab.url.startsWith("about:") &&
-			!tab.url.startsWith("chrome://") &&
-			!tab.url.startsWith("view-source:") &&
-			!tab.title.startsWith("https://") &&
-			!tab.title.startsWith("http://"),
-	)
+	const filteredTabs = tabs
+		.filter(
+			(tab) =>
+				tab.url &&
+				tab.url.length > 1 &&
+				!tab.url.startsWith("about:") &&
+				!tab.url.startsWith("chrome://") &&
+				!tab.url.startsWith("view-source:") &&
+				!tab.title.startsWith("https://") &&
+				!tab.title.startsWith("http://"),
+		)
+		.slice(0, 10)
 
 	return { tabs: filteredTabs }
 }
